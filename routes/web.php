e006a740d31ada2534a26987a5ea89ab5b4af425
Noe <noedalilis123@gmail.com>
2024-03-10 00:55:15 +0800

    creating controllers

--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,13 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
+
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('/name', function () {
-    return view('name');
-});
-Route::get('/number', function () {
-    return view('info.number');
-});
+Route::get('/', [DashboardController::class, 'index']);
+
+Route::get('/profile', [ProfileController::class, 'index']);
