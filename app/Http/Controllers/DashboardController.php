e8713bddeb66b8cae0b95a48022eaecef6878990
Noe <noedalilis123@gmail.com>
2024-03-10 02:10:16 +0800

    using blade with htmls tags

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -6,8 +6,28 @@ use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-    public function index (){
+    public function index ()
+    {
+        $users = [
+            [
+                'name' => 'Noe',
+                'age' => 10,
+            ],
+            [
+                'name' => 'Hero',
+                'age' => 11,
+            ],
+            [
+                'name' => 'John',
+                'age' => 18,
+            ]
+            ];
 
-        return view('welcome');
+        return view(
+            'dashboard',
+            [
+                'users' =>$users
+            ]
+        );
     }
 }
